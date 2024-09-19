bdb7a6fb89e6e8c727f4a8e0b7e9f8c65771c210
thouseplp <100323497+thouseplp@users.noreply.github.com>
2024-09-19 16:54:56 +0000

    All

--- pages/Appointments.py
+++ pages/Appointments.py
@@ -1,3 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from snowflake.snowpark import Session
+from snowflake.snowpark.functions import col
+
 st.set_page_config(
     page_title="Appointment Dashboard",
     #page_icon="",
